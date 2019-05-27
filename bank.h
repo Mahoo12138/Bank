502a6bee5b3944d88d220dab322d17d2000b651d
Mahoo12138 <Mahoo12138@qq.com>
2019-05-27 21:00:55 +0800

    添加用户列表及删除用户方法； 优化整体布局，修复少量bug

--- bank.h
+++ bank.h
@@ -69,6 +69,87 @@ int CheckInput(int key,string str) {
 	}
 }
 
+//读取指定内容数据行数
+int GetDataLine(string line) {
+	fstream file("SignUp.txt", ios::in);		//以只读方式打开文件
+	int n = 0;
+	string str;
+	while (1) {
+		getline(file, str, '\n');
+		n++;
+		if (str == line)
+			break;
+	}
+	file.close();
+	return n;
+}
+
+//修改指定行数据	原理类似于删除字符串
+void ChangeLineData(int lineNum, string content) {
+	ifstream in;
+	char line[1024] = { '\0' };
+	in.open("SignUp.txt");
+	int i = 0;
+	string tempStr;
+	while (in.getline(line, sizeof(line))) {
+		i++;
+		if (lineNum != i) {
+			tempStr += string(line);
+		}
+		else {
+			tempStr += content;
+		}
+		tempStr += '\n';
+	}
+	in.close();
+	ofstream out;
+	out.open("SignUp.txt");
+	out.flush();
+	out << tempStr;
+	out.close();
+}
+
+//读取指定行内容	
+char* ReadLineData(int lineNum) {
+	char data[1024];
+	ifstream in;
+	in.open("SignUp.txt");
+	int line = 1;
+	while (in.getline(data, 1024)) {
+		if (lineNum == line) {
+			break;
+		}
+		line++;
+	}
+	in.close();
+	return data;
+}
+
+//删除指定行数据	原理：一次读取文件一行，存入字符串，直到读到指定行数，将其转为空字符串存入字符串，再将字符串写入文件
+void DelLineData(int lineNum) {
+	ifstream in;
+	in.open("SignUp.txt");
+	string strFileData = "";
+	int line = 1;
+	char lineData[1024] = { 0 };
+	while (in.getline(lineData, sizeof(lineData))) {
+		if (line == lineNum) {
+			strFileData += "";
+		}
+		else {
+			strFileData += string(lineData);
+			strFileData += "\n";
+		}
+		line++;
+	}
+	in.close();
+	ofstream out;
+	out.open("SignUp.txt");
+	out.flush();
+	out << strFileData;
+	out.close();
+}
+
 //用户类，处理数据
 class User {
 private:
@@ -206,8 +287,8 @@ public:
 		GotoPosXY(10, 39);cout << "   ┍━━━━━━━━━━━┑";
 		GotoPosXY(11, 42);cout << "│           │";
 		GotoPosXY(12, 39);cout << "   ┕━━━━━━━━━━━┙";
-
-		GotoPosXY(11, 44);cin >> name;
+		cin.ignore();
+		GotoPosXY(11, 44);getline(cin,name);
 		
 		Yuser.Setname(name);
 
@@ -436,7 +517,7 @@ public:
 					GotoPosXY(19, 10);cout << "                             ";
 					GotoPosXY(19, 10); cout << "您的余额为：" << Yuser.Getmoney(); 
 				break;
-			case '6':Exit(); MainPage(); 
+			case '6':Exit(); 
 				break;
 
 			}
@@ -535,8 +616,11 @@ public:
 	//保存数据方法
 	void KeepData() {
 		int line = GetDataLine(Yuser.Getphonenum());
+		cout << line;
 		ChangeLineData(line + 1, Yuser.Getpassword());
+		cout << to_string(Yuser.Getmoney());
 		ChangeLineData(line + 2, to_string(Yuser.Getmoney()));
+
 	}
 	
 	//退出账户方法
@@ -570,8 +654,27 @@ public:
 
 	//管理员界面方法
 	void AdminPage() {
-		char x;
+		char ad;
 		string adminpw;
+		int i = 0, Userlocation[256] = { 0 },deluser;
+		char tempstr[256];
+		string tempStr;
+		fstream show("SignUp.txt");
+
+		//获取每个用户的数据位置
+		while (1) {
+			while (!show.eof()) {						//检索文件，读取数据
+				show.getline(tempstr, 256, '\n');
+				tempStr = string(tempstr);
+				if (CheckInput(b, tempStr)) {
+					Userlocation[i] = GetDataLine(tempStr);
+					i++;
+				}
+			}
+			show.close();
+			break;
+		}
+
 		RefreshPage();
 		GotoPosXY(6, 23);cout << "老黄银行后台账户管理系统";
 		GotoPosXY(14, 13);cout << "Copyright C 2019 老黄银行 All Rights Reserved.";
@@ -583,7 +686,7 @@ public:
 		while (1) {
 			GotoPosXY(11, 46);cin >> adminpw;
 			if (adminpw == admin_password) {
-				GotoPosXY(19, 16);cout << "登录成功！正在跳转界面……";
+				GotoPosXY(19, 10);cout << "登录成功！正在跳转界面……";
 				Sleep(800);
 				break;
 			}
@@ -603,126 +706,54 @@ public:
 		GotoPosXY(12, 24);cout << "                                 ";
 		
 		GotoPosXY(10, 23);cout << "1>  用户列表	 2>  新增用户";
-		GotoPosXY(12, 23);cout << "3>  删除用户	 4>  continue";
-		GotoPosXY(19, 30); cin >> x;
+		GotoPosXY(12, 23);cout << "3>  删除用户	 4>  退出系统";
+		GotoPosXY(19, 30); cin >> ad;
 		while (1) {
-			switch (x) {
-			case '1':UserList();
+			switch (ad) {
+			case '1':UserList(i,Userlocation);
 				break;
-			case '2':Withdraw();
+			case '2':		;
 				break;
-			case '4':
+			case '3': GotoPosXY(19, 10); cout << "请输入被删除用户序号：     ";
+				GotoPosXY(19, 40); cin >> deluser; DelUser(Userlocation[deluser-1]); UserList(--i, Userlocation);
+				break;
+			case '4':Exit();
 				break;
 
 			}
-			GotoPosXY(20, 10);
-			cout << "请根据需求输入您的下一步操作：     ";
-			GotoPosXY(20, 34);
-			cin >> x;
+			GotoPosXY(19, 10);cout << "请根据需求输入您的下一步操作：     ";
+			GotoPosXY(19, 40);cin >> ad;
 		}
-
 		system("pause");
 	}
 
 	//用户列表实现
-	void UserList() {
-		GotoPosXY(4, 66); cout <<  "※※※※※※※※※※※※※※※※※※※※※※※※※";
-		GotoPosXY(5, 66); cout <<  "※                                              ※";
-		GotoPosXY(6, 66); cout <<  "※               老黄银行已注册用户             ※";
-		GotoPosXY(7, 66); cout <<  "※                                              ※";
-		GotoPosXY(8, 66); cout <<  "※ 姓名   手机号       金额        注册时间     ※";
-		GotoPosXY(9, 66); cout <<  "※                                              ※";
-		GotoPosXY(10, 66); cout << "※                                              ※";
-		GotoPosXY(11, 66); cout << "※                                              ※";
-		GotoPosXY(12, 66); cout << "※                                              ※";
-		GotoPosXY(13, 66); cout << "※                                              ※";
-		GotoPosXY(14, 66); cout << "※                                              ※";
-		GotoPosXY(14, 66); cout << "※                                              ※";
-		GotoPosXY(15, 66); cout << "※※※※※※※※※※※※※※※※※※※※※※※※※";
-
-		int i = 0 ,Userlocation[256] = {0};
-		char tempstr[256];
-		string tempStr;
-		fstream show("SignUp.txt");
-
-		//获取每个用户的数据位置
-		while (1) {
-			while (!show.eof()) {						//检索文件，读取数据
-				show.getline(tempstr, 256, '\n');
-				tempStr = string(tempstr);
-				if (CheckInput(b,tempStr)) {
-					Userlocation[i] = GetDataLine(tempStr);
-					i++;
-				}
-			}
-			show.close();
-			break;
+	void UserList(int i,int* Userlocation) {		//'i'为系统已注册用户，Userlocation为存储每个用户手机号数据行号的数组
+		GotoPosXY(4, 62); cout <<  "※※※※※※※※※※※※※※※※※※※※※※※※※※";
+		GotoPosXY(5, 62); cout <<  "※                                                ※";
+		GotoPosXY(6, 62); cout <<  "※               老黄银行已注册用户               ※";
+		GotoPosXY(7, 62); cout <<  "※                                                ※";
+		GotoPosXY(8, 62); cout <<  "※ 序号 姓名    手机号      金额     注册时间     ※";
+		GotoPosXY(9, 62); cout <<  "※                                                ※";
+		for (int lines = 0; lines < i;lines++) {
+			GotoPosXY(10 + lines, 62); cout << "※                                                ※";
 		}
+		GotoPosXY(10 + i, 62); cout << "※※※※※※※※※※※※※※※※※※※※※※※※※※";
 
 		for (int j = 0, x = 9, y = 69; j < i; j++,x++) {
-			GotoPosXY(x, y); cout << ReadLineData(Userlocation[j] + 3);
-			GotoPosXY(x, y + 7); cout << ReadLineData(Userlocation[j]);
-			GotoPosXY(x, y + 20); cout << ReadLineData(Userlocation[j] + 2);
-			GotoPosXY(x, y + 32); cout <<  ReadLineData(Userlocation[j] -1);
-		}
-		
-		
-	}
-		
-	//读取指定内容数据行数
-	int GetDataLine(string line) {
-		fstream file("SignUp.txt",ios::in);		//以只读方式打开文件
-		int n = 0;
-		string str;
-		while (1){
-			getline(file, str, '\n');
-			n++;
-			if (str == line)
-			break;
-		}
-		file.close();
-		return n;
+			GotoPosXY(x, y - 4); cout << j+1 <<">"; Sleep(200);	//输出序号
+			GotoPosXY(x, y); cout << ReadLineData(Userlocation[j] + 3); Sleep(200);	//输出姓名
+			GotoPosXY(x, y + 9); cout << ReadLineData(Userlocation[j]); Sleep(200);	//输出手机号
+			GotoPosXY(x, y + 21); cout << ReadLineData(Userlocation[j] + 2); Sleep(200);	//输出金额
+			GotoPosXY(x, y + 30); cout <<  ReadLineData(Userlocation[j] -1); Sleep(200);	//输出注册时间
+			Sleep(200);
+		}	
 	}
 
-	//修改指定行数据
-	void ChangeLineData(int lineNum, string content) {
-		ifstream in;
-		char line[1024] = { '\0' };
-		in.open("SignUp.txt");
-		int i = 0;
-		string tempStr;
-		while (in.getline(line, sizeof(line))) {
-			i++;
-			if (lineNum != i) {
-				tempStr += string(line);
-			}
-			else {
-				tempStr += content;
-			}
-			tempStr += '\n';
-		}
-		in.close();
-		ofstream out;
-		out.open("SignUp.txt");
-		out.flush();
-		out << tempStr;
-		out.close();
-	}
-
-	//读取指定行内容
-	char* ReadLineData(int lineNum) {
-		char data[1024];
-		ifstream in;	
-		in.open("SignUp.txt"); 	
-		int line = 1;	
-		while (in.getline(data, 1024)) { 
-			if (lineNum == line) { 
-				break; 
-			}		
-			line++; 
-		} 	
-		in.close(); 
-		return data;
+	//删除用户方法实现
+	void DelUser(int line) {
+		for(int i=0;i<5;i++)
+		DelLineData(line - 1);
 	}
 
 	//转账：搜索用户，读取数据
